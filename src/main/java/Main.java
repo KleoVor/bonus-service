0e7086608d5d346b2a8768be95000aaae18479f7
KleoVor <t9671253993@gmail.com>
2023-02-06 17:54:25 +0300

    Fix bug, fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
+    // проверка закрытия commit-ом issue
     System.out.println(service.calculate(amount, registered));
   }
 }
